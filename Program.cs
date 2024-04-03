fab9a8aa36af412e59b8e123734a5da670e20e61
Naila <khalfaguliyevanaila@gmail.com>
2024-04-03 11:59:36 +0400

    second change by Naila

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
 ﻿Console.WriteLine("Test Github ");
+
+
+Console.WriteLine("Second Test Github ");
+
+//neise deisdik misal ucun 
+
+//deir program.cs deisilib onun ucun git add . edecem saxlayacaq yaddasa
